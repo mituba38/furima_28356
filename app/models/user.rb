1a00077ab05491534af67c8475bec4200ebbc99a
mituba38 <68590316+mituba38@users.noreply.github.com>
2020-08-21 17:15:39 +0900

    バリデーションの修正　ログイン時ログアウト時のビューの切り替え機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,22 @@
 class User < ApplicationRecord
 
-  with_options presence: true do
-    validataes :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-    validataes :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-    validataes :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
-    validataes :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+
+    validates :first_name,presence: true
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角で入力してください。"}
+    validates :last_name,presence: true
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角で入力してください。"}
+    validates :first_name_kana,presence: true
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+    validates :last_name_kana,presence: true
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+    validates :birth_date,presence: true
 
 
 
   # Include default devise modules. Others available are:
   
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+ 
 end
