f86ac48562a1fa4810da8c74c1d7534af3463680
mituba38 <68590316+mituba38@users.noreply.github.com>
2020-08-24 17:57:49 +0900

    rubocopによる修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,24 @@
 class User < ApplicationRecord
-
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
 
+  # validates :email, uniqueness: true
+  # validates :email, format: {　with: /\A\S+@\S+\.\S+\z/, message: "は@を含めた半角英数字で入力して下さい" }
+  validates :password, length: { minimum: 6, too_short: 'は最低６文字必要です' }
 
-    # validates :email, uniqueness: true
-    # validates :email, format: {　with: /\A\S+@\S+\.\S+\z/, message: "は@を含めた半角英数字で入力して下さい" }
-    validates :password, length: { minimum: 6,too_short:"は最低６文字必要です"}
-
-    validates :nickname,presence: true
-    validates :first_name,presence: true
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角で入力してください。"}
-    validates :last_name,presence: true
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角で入力してください。"}
-    validates :first_name_kana,presence: true
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
-    validates :last_name_kana,presence: true
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
-    validates :birth_date,presence: true
-    # validates :passwadd,
-
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角で入力してください。' }
+  validates :last_name, presence: true
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角で入力してください。' }
+  validates :first_name_kana, presence: true
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。' }
+  validates :last_name_kana, presence: true
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。' }
+  validates :birth_date, presence: true
+  # validates :passwadd,
 
   # Include default devise modules. Others available are:
-  
+
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
- 
 end
