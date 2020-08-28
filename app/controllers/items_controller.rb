25bf63fc73fc5f58dc1d95918acab29e953eba9b
mituba38 <68590316+mituba38@users.noreply.github.com>
2020-08-28 12:30:44 +0900

    active hashの導入　地域表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
   def index
   end
+  private
+
+  def item_params
+    params.require(:item).permit(:user, :image, :explanation, :name, :price, :category, :status, :charge, :area, :date).merge(user_id: current_user.id)
+  end
  end
