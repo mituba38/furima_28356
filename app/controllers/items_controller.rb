6494e9db1fd5440d45292bf02322e8542a52859f
mituba38 <68590316+mituba38@users.noreply.github.com>
2020-08-31 21:58:36 +0900

    商品リンクの修正、画像価格商品名の表示、商品がない場合は見本を表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,14 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
     @item = Item.new
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def create
