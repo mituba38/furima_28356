89993cce6e62c5fc680897e4bf388a7a773ee835
mituba38 <68590316+mituba38@users.noreply.github.com>
2020-09-01 14:59:40 +0900

    商品情報編集機能''

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit,:update,:destroy,:show]
+  before_action :authenticate_user!, only: [:edit,:update,:destroy]
   def index
     @items = Item.all
   end
