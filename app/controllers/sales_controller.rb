b572132c75959e2f4f734cfeed5684540a93a5e1
mituba38 <68590316+mituba38@users.noreply.github.com>
2020-09-02 15:33:35 +0900

    pay.jpの導入、コントローラーの設定

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,8 +2,31 @@ class SalesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end  
+
   def create
-    @sale = Sale.new(sale_params)
+    @sale = Sale.new(price: sale_params[:price])
+    if @sale.valid?
+      pay_item
+      @sale.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end  
+
+  private
+
+  def order_params
+    params.permit(:price, :token)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_4ee6f7d5e208a5a9e3b14622"  
+    Payjp::Charge.create(
+      amount: sale_params[:price],  
+      card: sale_params[:token],    
+      currency:'jpy'                 
+    )
+  end
    
 end
