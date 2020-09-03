9cfa92ba8cbf3788877f6c9b321b008d0ef5ee1f
mituba38 <68590316+mituba38@users.noreply.github.com>
2020-09-03 15:31:30 +0900

    バリデーションの記入

--- app/forms/sale_adress.rb
+++ app/forms/sale_adress.rb
@@ -5,14 +5,14 @@ class SaleAdress
   
   with_options presence: true do
     validates :prefecture_id, numericality: { other_than: 0 }
-    # validates :token
-    # validates :user_id
-    # validates :postal_code
-    # validates :prefecture_id
-    # validates :city
-    # validates :address
-    # validates :building
-    # validates :phone_number
+    validates :token
+    validates :user_id
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/}
+    validates :prefecture_id
+    validates :city
+    validates :address
+    validates :building
+    validates :phone_number, format: { with: /\A0\d{9,10}\z/}
   end  
   def save
     sale = Sale.create(user_id: user_id, item_id: item_id)
